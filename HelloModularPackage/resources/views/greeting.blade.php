e09132888ba47613032e51de4d05162b61f97306
WendyWang <w5569590@gmail.com>
2025-03-28 13:46:29 +0800

    Delete HelloModularPackage directory

--- HelloModularPackage/resources/views/greeting.blade.php
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head><title>Hello Modular</title></head>
-<body>
-    <h1>Hello from Modular View!</h1>
-</body>
-</html>
--- HelloModularPackage/src/routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/hellomodular', function () {
-    return view('hellomodular::greeting');
-});
